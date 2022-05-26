4a07b25c4ddcf3dce117de93a7b124a1773524ca
Sarah West <west0883@umn.edu>
2022-05-26 09:48:46 -0500

    Code to save Arduino logs as Matlab cells.

---
+++ extractMotorData.m
@@ -0,0 +1,61 @@
+% extractMotorData.m
+% Sarah West
+% 11/16/21
+
+% Function that takes the output files from the Arduino serial monitor
+% during motorized treadmill experiements and saves data as .mat files.
+
+function [] = extractMotorData(parameters)
+
+    mice_all = parameters.mice_all;
+    dir_dataset_name = parameters.dir_dataset_name;
+    input_data_name = parameters.input_data_name;
+    dir_exper = parameters.dir_exper; 
+    digitNumber = parameters.digitNumber; 
+    putty_flag = parameters.putty_flag;
+    
+    % Establish base output directory
+    dir_out_base=[dir_exper 'behavior\extracted motor data\'];
+    
+    % Tell user where data is being saved
+    disp(['Data saved in ' dir_out_base]); 
+    
+    % For each mouse 
+    for mousei=1:size(mice_all,2)
+        mouse=mice_all(mousei).name;
+        
+        % For each day
+        for dayi=1:size(mice_all(mousei).days, 2)
+            
+            % Get the day name.
+            day=mice_all(mousei).days(dayi).name; 
+            
+            % Create data input directory and cleaner output directory. 
+            dir_in=CreateFileStrings(dir_dataset_name, mouse, day, [], [], false);
+            dir_out=[dir_out_base mouse '\' day '\']; 
+            mkdir(dir_out); 
+            
+            % Get the stack list
+            [stackList]=GetStackList(mousei, dayi, parameters);
+            
+            % If not using PUTTY
+            if ~putty_flag 
+                
+                % Cycle through the stack files. 
+                for stacki=1:size(stackList.filenames,1)
+
+                    % Get the stack number and filename for the stack.
+                    stack_number = stackList.numberList(stacki, :);
+                    filename = stackList.filenames(stacki, :);
+                    
+                    % Import. The readtext function is amazing.
+                    trial = readtext([dir_in filename]);
+                    
+                    % Save
+                    save([dir_out 'trial' stack_number '.mat'], 'trial', '-v7.3');
+
+                end
+            end
+        end
+    end
+end 
--- pipeline_motorized_treadmill_behavior.m
+++ pipeline_motorized_treadmill_behavior.m
@@ -12,7 +12,7 @@ clear all;
 % Output Directories
 
 % Create the experiment name. This is used to name the output folder. 
-parameters.experiment_name='Random Motorized Treadmill';
+parameters.experiment_name='Random Motorized Treadmill\test data';
 
 % Output directory name bases
 parameters.dir_base='Y:\Sarah\Analysis\Experiments\';
@@ -44,16 +44,17 @@ parameters.mice_all = mice_all;
 % number', 'day', or 'stack number' where the mouse, day, or stack number 
 % will be. If you concatenated this as a sigle string, it should create a 
 % file name, with the correct mouse/day/stack name inserted accordingly. 
-parameters.dir_dataset_name={'Y:\Sarah\Data\', parameters.experiment_name, '\', 'day', '\','m', 'mouse number', '\'};
-parameters.input_data_name={'ArduinoOutput*.log' }; 
+parameters.dir_dataset_name={['Y:\Sarah\Data\Motorized Treadmill'], '\', 'day', '\', 'mouse number', '\Arduino output\'};
+parameters.input_data_name={'stack number', '.txt' }; 
 
 % Give the number of digits that should be included in each stack number.
-parameters.digitNumber=2; 
+parameters.digitNumber=3; 
 
 % *************************************************************************
 % Parameters
 
-% Sampling frequency of wheel encoder, in Hz.
+% Sampling frequency of serial monitor, in Hz. Arduino displays time in ms,
+% so is almost always 1000 Hz.
 parameters.wheel_Hz = 1000;
 
 % Sampling frequency of collected brain data (per channel), in Hz or frames per
@@ -73,61 +74,15 @@ parameters.frames=6000;
 parameters.skip = 1200; 
 
 % Was PUTTY used for the recording? 
-parameters.putty_flag = true;
+parameters.putty_flag = false;
 
 % Radius of wheel, in cm.
 parameters.wheel_radius = 8.5;
-                     
-% List periods that are long/will need to be cut into smaller sections.
-% Rest and walk are the 2 primary behaviors detected by the encoder, and
-% need to be detected first.
-parameters.periods_long={'rest';
-              'walk'};
-          
-% List periods that are transition periods that are taken from pieces of
-% rest and walk. The "continued rest" and "continued walk" is the rest and
-% walk that is left after the transition periods are removed. 
-parameters.periods_transition={'startwalk';     % Run the within-walk periods first,so you can make sure there's a real "walking" period after the prewalk, not just a large fidget
-                'prewalk'; 
-                'stopwalk';
-               'postwalk'}; 
-                             
-% List the threshold speeds of each long period (lower, upper) in cm/s;
-% Values that fall between the 0.05 and 0.25 are probably twitches or
-% fidgets. 
-parameters.periods_long_threshold=[ -0.25 0.05;  % rest low, rest high. We don't want to count walking fast backwards.
-                         0.25 Inf];   % walk low, walk high
-                     
-% List the search orders for finding the earliest or most recent velocity
-% of 0 to define the start and stop of rest or locomotion. This makes more
-% sense when you look at the code that uses it (encoderFindBehaviorPeriods). Probably won't need to edit. 
-parameters.periods_long_searchorder={'first', 'last';    % rest start, rest stop
-                          'last', 'first'};   % walk start, walk stop
-                      
-% ***Analysis info ****
-% Number of time points to smooth the vel by 
-parameters.k=100; 
-% How the animal needs to be at rest or walking for it to count, in seconds
-parameters.time_window_seconds=3; 
-
-parameters.time_window_frames=parameters.time_window_seconds*parameters.fps; % how long the animal needs to be at rest or walking, in frames
-parameters.time_window_hz=parameters.time_window_seconds*parameters.wheel_Hz; % how long the animal needs to be at rest or walking, in wheel sampled time points
-
-% Do you want full transitions? (These are rare)
-% "true" if you want "full onsets" and "full offsets" calculated; false if
-% not.
-parameters.full_transition_flag = true;  
-parameters.periods_full_transition={'full_onset';
-                         'full_offset'}; 
-
-% In seconds, the amount of time into the continued rest and walk that should be included in the full transition                         
-parameters.full_transition_extra_time=1;                  
-parameters.full_transition_extra_hz=parameters.full_transition_extra_time*parameters.wheel_Hz;                                          
+                                  
              
 %% Extract data and save as .mat file.  (Can take awhile).
 % From .log if PUTTY was used, from .txt files if it wasn't. 
-parameters.input_data_name={'ArduinoOutput*.log' }; 
-extractEncoderData(parameters);
+extractMotorData(parameters);
 
 %% Clean and format data. (Can take awhile).
 
